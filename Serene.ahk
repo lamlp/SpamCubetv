8662467709300d96a98861c47b6bc4a3a5df5fc1
nnlam94 <nnlam94@gmail.com>
2019-10-19 16:54:15 +0700

    done

---
+++ Serene.ahk
@@ -0,0 +1,143 @@
+﻿RequireAdmin()
+#NoEnv
+#SingleInstance Force
+playing:=0
+SetWorkingDir %A_ScriptDir%
+CoordMode Mouse,Window
+SetControlDelay -1
+
+MsgBox Made by Serene
+
+Gui,+Alwaysontop
+Gui Add, Text, x16 y8, Hành động:
+Gui Add, DropDownList, x81 y5 w120 vModeClick, Left||Middle|Right|Text
+Gui Add, Text, x215 y8 ,Delay:
+Gui Add, Edit, x250 y5 w120 h21 vDelayTime number,100
+Gui Add, Text, x374 y11 ,(ms)
+Gui Add, Text, x16 y32 w60 h23 +0x200, Số lần chạy: ; Đặt 0 để chạy vô hạn lần
+Gui Add, Edit, x81 y34 w65 h21 number vCount,
+Gui Add, UpDown, x128 y32 w18 h21, 1
+Gui Add, ListView, x16 y62 w391 h150 vMyListView, X|Y|Hành động|Delay|Cửa sổ
+Gui Add, Button, x152 y222 w80 h23 gRunAuto , Chạy (F3)
+Gui Add, Button, x240 y224 w80 h23 gStopAuto , Dừng (F4)
+Gui Add, Button, x328 y224 w80 h23 gDellList, Xóa
+Gui Show, , Serene
+
+Menu, MyContextMenu, Add, Xóa, DellList
+Menu, MyContextMenu, Add, Xóa tất cả, DellList
+
+Return
+
+F2::
+if playing
+   return
+Gui,Submit,Nohide
+MouseGetPos,,,WinHWND
+WinActivate,ahk_id %WinHWND%
+MouseGetPos,X,Y,
+LV_Add("",X,Y,ModeClick,DelayTime,WinHWND)
+LV_ModifyCol(1)
+LV_ModifyCol(2)
+return
+
+
+F3::
+RunAuto:
+Dem:=0
+Chay:=0
+fileline:=0
+Loop, Read, abc.txt
+{
+   total_lines = %A_Index%
+}
+Gui,Submit,Nohide
+if (LV_GetCount()=0)
+   return
+if (Count=0)
+   Count=-1
+playing:=1
+Loop
+{
+   if (Dem=LV_GetCount())
+   {
+     Dem:=0
+     Chay++
+   }
+   if (Stop || Count=Chay)
+     break
+   Dem++
+   Loop % LV_GetCount("Col")
+     LV_GetText(Data_%A_Index%,Dem,A_Index)
+   Sleep,% Data_4
+   if Stop
+     break
+   Temp=%Data_3%
+   if (Temp="Text")
+   {
+   fileline++
+   FileReadLine, line, abc.txt, fileline
+   if ErrorLevel
+        break
+   Send, %line%
+   if (fileline=total_lines)
+   fileline:=0
+   }
+   else
+   ControlClick,x%Data_1% y%Data_2%,ahk_id %Data_5%,,%Data_3%,,NA
+
+}
+stop:=0
+playing:=0
+return
+
+F4::
+StopAuto:
+if not playing
+   return
+Stop:=1
+playing:=0
+return
+
+GuiContextMenu:
+if A_GuiControl <> MyListView
+  return
+Menu, MyContextMenu, Show, %A_GuiX%, %A_GuiY%
+return
+
+
+DellList:
+RowNumber = 0
+if (A_ThisMenuItem="Xóa")
+Loop
+{
+  RowNumber := LV_GetNext(RowNumber - 1)
+  if not RowNumber
+  break
+  LV_Delete(RowNumber)
+}
+else
+   LV_Delete()
+return
+
+
+GuiEscape:
+GuiClose:
+Esc::
+  ExitApp
+
+RequireAdmin()
+{
+CommandLine := DllCall("GetCommandLine", "Str")
+
+If !(A_IsAdmin || RegExMatch(CommandLine, " /restart(?!\S)")) {
+    Try {
+        If (A_IsCompiled) {
+            Run *RunAs "%A_ScriptFullPath%" /restart
+        } Else {
+            Run *RunAs "%A_AhkPath%" /restart "%A_ScriptFullPath%"
+        }
+    }
+    ExitApp
+}
+}
+ 
---
+++ WindowsFormsApplication3/WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem == "Nhóm 1")
+            {
+                Process.Start("https://www.cube.tv/29983426");
+                Process.Start("https://www.cube.tv/30020593");
+                Process.Start("https://www.cube.tv/29988566");
+                Process.Start("https://www.cube.tv/29988574");
+            }
+            else if (comboBox1.SelectedItem == "Nhóm 2")
+            {
+                Process.Start("https://www.cube.tv/29988579");
+                Process.Start("https://www.cube.tv/29988580");
+                Process.Start("https://www.cube.tv/29988567");
+                Process.Start("https://www.cube.tv/29956662");
+            }
+            else if (comboBox1.SelectedItem == "Nhóm 3")
+            {
+                Process.Start("https://www.cube.tv/29956663");
+                Process.Start("https://www.cube.tv/29988616");
+                Process.Start("https://www.cube.tv/29956660");
+                Process.Start("https://www.cube.tv/29956661");
+            }
+            else if (comboBox1.SelectedItem == "Nhóm 4")
+            {
+                Process.Start("https://www.cube.tv/29956674");
+                Process.Start("https://www.cube.tv/29956673");
+                Process.Start("https://www.cube.tv/29956675");
+                Process.Start("https://www.cube.tv/29956677");
+            }
+            else if (comboBox1.SelectedItem == "Nhóm 5")
+            {
+                Process.Start("https://www.cube.tv/29956672");
+                Process.Start("https://www.cube.tv/29988678");
+                Process.Start("https://www.cube.tv/29956722");
+                Process.Start("https://www.cube.tv/29956725");
+            }
+            else MessageBox.Show("Chọn Nhóm trước đã");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem=="Nhóm 1")
+            {
+                richTextBox1.Text = "https://www.cube.tv/29983426\nhttps://www.cube.tv/30020593\nhttps://www.cube.tv/29988566\nhttps://www.cube.tv/29988574";
+            }
+            if (comboBox1.SelectedItem == "Nhóm 2")
+            {
+                richTextBox1.Text = "https://www.cube.tv/29988579\nhttps://www.cube.tv/29988580\nhttps://www.cube.tv/29988567\nhttps://www.cube.tv/29956662";
+            }
+            if (comboBox1.SelectedItem == "Nhóm 3")
+            {
+                richTextBox1.Text = "https://www.cube.tv/29956663\nhttps://www.cube.tv/29988616\nhttps://www.cube.tv/29956660\nhttps://www.cube.tv/29956661";
+            }
+            if (comboBox1.SelectedItem == "Nhóm 4")
+            {
+                richTextBox1.Text = "https://www.cube.tv/29956674\nhttps://www.cube.tv/29956673\nhttps://www.cube.tv/29956675\nhttps://www.cube.tv/29956677";
+            }
+            if (comboBox1.SelectedItem == "Nhóm 5")
+            {
+                richTextBox1.Text = "https://www.cube.tv/29956672\nhttps://www.cube.tv/29988678\nhttps://www.cube.tv/29956722\nhttps://www.cube.tv/29956725";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var lines = File.ReadAllLines("abc.txt");
+            var rnd = new Random();
+            lines = lines.OrderBy(line => rnd.Next()).ToArray();
+            File.WriteAllLines("abc.txt", lines);
+            MessageBox.Show("Đã trộn xong");
+        }
+
+        public void ExecuteAsAdmin(string fileName)
+        {
+            Process proc = new Process();
+            proc.StartInfo.FileName = fileName;
+            proc.StartInfo.UseShellExecute = true;
+            proc.StartInfo.Verb = "runas";
+            proc.Start();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ExecuteAsAdmin("Serene.exe");
+        }
+    }
+}
